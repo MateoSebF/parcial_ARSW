9a4185823b4239792ab9bfb6d95e6398766bfb01
unknown <mateo.forero-f@labinfo.is.escuelaing.edu.co>
2025-02-21 10:18:07 -0500

    base proyect

---
+++ src/main/java/edu/eci/arsw/parcial/Parcial.java
@@ -0,0 +1,12 @@
+package edu.eci.arsw.parcial;
+
+/**
+ *
+ * @author mateo.forero-f
+ */
+public class Parcial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
